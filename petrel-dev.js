19c069dd25cc5f8348df75e1c7e643a96b2eacc1
Shawn Boyette <shawn@firepear.net>
2016-11-01 23:21:53 -0600

    removing attempted send-wait machinery, in prep for proper callback design

--- petrel-dev.js
+++ petrel-dev.js
@@ -13,14 +13,6 @@ function petrelReceive(obj, event) {
     obj.respq.push(event.data);
 }
 
-// petrelSleep is a utility function which returns a promise that
-// resolves itself in a given number of milliseconds. It's called by
-// petrelReceive to implment a sleep while waiting on responses to be
-// populated.
-function petrelSleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 // petrelDispatch sends a request over the network. It takes one
 // argument, the request itself. It returns the response
 // value. this.error should always be checked after calling Dispatch.
@@ -47,27 +39,6 @@ function petrelDispatch(request) {
     return this.Response();
 }
 
-// petrelResponse is called by petrelDispatch and attempts to get the
-// first value from respq. If respq has no members for the length of
-// time defined by the client's timeout, it returns null.
-function petrelResponse() {
-        var totalsleep = 0;
-        if this.respq.length > 0 {
-            return this.respq.shift();
-        }
-        while (this.timeout > 0 && totalsleep < this.timeout) {
-            // FUTURE        await petrelSleep(25);
-            yeild petrelSleep(25);
-            totalsleep = totalsleep + 25;
-            if this.respq.length > 0 {
-                return this.respq.shift();
-            }
-        }
-        this.errq.push('timed out waiting on response');
-        this.ws.close();
-        this.error = true;
-}
-
 function petrelError() {
 }
 
@@ -92,6 +63,19 @@ function Petrel(address, timeout, secure, hmac) {
     this.hmac = hmac || null;
     this.timeout = timeout || 0;
 
+    // reqq is the request queue. When a request is dispatched,
+    // information about it is stored here for retrieval when the
+    // websocket callback fires.
+    this.reqq = new Array();
+
+    // errq holds any error messages which are generated during
+    // operation. Error uses it to produce a traceback message.
+    this.errq = new Array();
+
+    this.Dispatch = petrelDispatch;
+    this.Receive = petrelReceive;
+    this.Error = petrelError;
+
     // instantiate websocket
     if (secure == true) {
         this.address = 'wss://' + address;
@@ -106,17 +90,5 @@ function Petrel(address, timeout, secure, hmac) {
         this.error = true;
     }
 
-    // respq is the response queue. Since websockets are async but
-    // Petrel servers are synchronous, we put data here as we get
-    // it. Calls to Response pull from here.
-    this.respq = new Array();
-
-    // errq holds any error messages which are generated during
-    // operation. Error uses it to produce a traceback message.
-    this.errq = new Array();
-
-    this.Dispatch = petrelDispatch;
-    this.Receive = petrelReceive;
-    this.Error = petrelError;
 }
 
