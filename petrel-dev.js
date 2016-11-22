c351135e3853729c0a57cc9d3b27c0afba19effd
Shawn Boyette <shawn@firepear.net>
2016-11-22 02:20:52 -0700

    uses ES6 classes and strict mode now

--- petrel-dev.js
+++ petrel-dev.js
@@ -18,131 +18,135 @@
 // When any error occurs, an exception will be thrown and the current
 // client will do no further work, even if that exception is trapped.
 // A new client should be instantiated, with a new websocket.
-function PetrelClient(timeout, hmac, ws) {
-    this.err = null;
-    this.hmac = hmac || null;
-    this.timeout = timeout || 0;
-    this.ws = ws;
-    this.seq = 0;
-    this.pver = 0;
+"use strict";
+class PetrelClient {
+    constructor(timeout, hmac, ws) {
+        this.err = null;
+        this.hmac = hmac || null;
+        this.timeout = timeout || 0;
+        this.ws = ws;
+        this.seq = 0;
+        this.pver = 0;
 
-    // reqq is the request "queue". When a request is dispatched,
-    // information about it is stored here for retrieval when the
-    // websocket callback fires.
-    this.reqq = new Object();
+        // reqq is the request "queue". When a request is dispatched,
+        // information about it is stored here for retrieval when the
+        // websocket callback fires.
+        this.reqq = new Object();
 
-    // assign methods
-    this.Dispatch = petrelDispatch;
-    this.error = petrelError;
-}
-
-// petrelDispatch sends a request over the network. It takes two
-// arguments: the request message, and the function which should be
-// called when the response is received.
-function petrelDispatch(request, callback) {
-    if (this.err != null) {
-        throw 'cannot dispatch request due to previous error';
+        // assign methods
+        //this.Dispatch = petrelDispatch;
+        //this.error = petrelError;
     }
 
-    // assemble full request
-    this.seq++;
-    try {
-        var msg = petrelMarshal(this, request);
-    } catch (e) {
-        this.error(e);
-        throw e;
-    }
-
-    // put req data into reqq
-    this.reqq[this.seq] = callback;
-
-    // send request
-    if (this.ws != undefined) {
-        try {
-            this.ws.send(msg);
+    // dispatch sends a request over the network. It takes two
+    // arguments: the request message, and the function which should
+    // be called when the response is received.
+    dispatch(request, callback) {
+        if (this.err != null) {
+            throw 'cannot dispatch request due to previous error';
         }
-        catch (e) {
-            this.error("couldn't send request: " + e);
+
+        // assemble full request
+        this.seq++;
+        try {
+            var msg = petrelMarshal(this, request);
+        } catch (e) {
+            this.error(e);
             throw e;
         }
-    }
-}
 
-// petrelError simply closes the ws and sets the client's err
-// attribute.
-function petrelError(errmsg) {
-    if (this.ws != undefined) {
-        this.ws.close();
+        // put req data into reqq
+        this.reqq[this.seq] = callback;
+
+        // send request
+        if (this.ws != undefined) {
+            try {
+                this.ws.send(msg);
+            }
+            catch (e) {
+                this.error("couldn't send request: " + e);
+                throw e;
+            }
+        }
     }
-    this.err = errmsg;
+
+    // error simply closes the ws and sets the client's err attribute.
+    error(errmsg) {
+        if (this.ws != undefined) {
+            this.ws.close();
+        }
+        this.err = errmsg;
+    }
 }
 
 
 
 // PetrelMsg represents a Petrel message.
-function PetrelMsg() {
-    this.seq = null;
-    this.plen = null;
-    this.pver = null;
-    this.payload = null;
-    this.hmac = null;
-    this.verifiedmac = false;
-    this.rebuild = msgRebuild;
-}
+class PetrelMsg {
+    constructor() {
+        this.seq = null;
+        this.plen = null;
+        this.pver = null;
+        this.payload = null;
+        this.hmac = null;
+        this.verifiedmac = false;
+    }
 
-// msgRebuild is a method of PetrelMsg instances. It is called by the
-// internal callbacks of petrelUnmarshal. It tests to see if all
-// attributes of a message are populated, and if so it invokes the
-// callback for its request.
-//
-// Unlike the original Go implementation, where request responders
-// only get the message payload, request callbacks in petreljs get the
-// entire message.
-function msgRebuild(p) {
-    if (p.err != null) {
-        return;
-    }
-    if (this.seq == null || this.plen == null || this.pver == null || this.payload == null) {
-        return;
-    }
-    // calculate and compare MAC if needed
-    if (this.hmac != null) {
-        var shaObj = new jsSHA('SHA-256', 'TEXT');
-        shaObj.setHMACKey(p.hmac, 'TEXT');
-        shaObj.update(this.payload);
-        var calchmac = shaObj.getHMAC('B64');
-        // jsSHA does not appear to have a safe compare function for
-        // HMACs, so we have to provide that, to avoid timing
-        // attacks.
-        var failed = false;
-        // we do 44 comparisons, one for each character in a HMAC-256
-        // expressed as Base64. this way the comparison should take
-        // roughly the same time, no matter what is passed to us. read
-        // about HMAC timing attacks if you want to know more.
-        for (var i = 0; i < 44; i++) {
-            // being undefined is a mismatch
-            if (this.hmac[i] == undefined || calchmac[i] == undefined) {
-                failed = true;
+    // rebuild is a method of PetrelMsg instances. It is called by the
+    // internal callbacks of petrelUnmarshal. It tests to see if all
+    // attributes of a message are populated, and if so it invokes the
+    // callbback for its request.
+    //
+    // Unlike the original Go implementation, where request responders
+    // only get the message payload, request callbacks in petreljs get
+    // the entire message.
+    rebuild(p) {
+        if (p.err != null) {
+            return;
+        }
+        if (this.seq == null || this.plen == null || this.pver == null || this.payload == null) {
+            return;
+        }
+        // calculate and compare MAC if needed
+        if (this.hmac != null) {
+            var shaObj = new jsSHA('SHA-256', 'TEXT');
+            shaObj.setHMACKey(p.hmac, 'TEXT');
+            shaObj.update(this.payload);
+            var calchmac = shaObj.getHMAC('B64');
+            // jsSHA does not appear to have a safe compare function
+            // for HMACs, so we have to provide that, to avoid timing
+            // attacks.
+            var failed = false;
+            // we do 44 comparisons, one for each character in a
+            // HMAC-256 expressed as Base64. this way the comparison
+            // should take roughly the same time, no matter what is
+            // passed to us. read about HMAC timing attacks if you
+            // want to know more.
+            for (var i = 0; i < 44; i++) {
+                // being undefined is a mismatch
+                if (this.hmac[i] == undefined || calchmac[i] == undefined) {
+                    failed = true;
+                }
+                // and of course a mismatch is a mismatch.
+                if (this.hmac[i] != calchmac[i]) {
+                    failed = true;
+                }
             }
-            // and of course a mismatch is a mismatch.
-            if (this.hmac[i] != calchmac[i]) {
-                failed = true;
+            if (failed == false) {
+                this.verifiedmac = true;
+            } else {
+                p.error('HMAC mismatch in request ' + this.seq);
             }
         }
-        if (failed == false) {
-            this.verifiedmac = true;
-        } else {
-            p.error('HMAC mismatch in request ' + this.seq);
+        // invoke the callback for this message
+        if (p.reqq[this.seq] == undefined) {
+            p.error('callback for request ' + this.seq + 'is undefined');
+            return;
         }
+        var responder = p.reqq[this.seq];
+        delete p.reqq[this.seq];
+        responder(this);
     }
-    // invoke the callback for this message
-    if (p.reqq[this.seq] == undefined) {
-        p.error('callback for request ' + this.seq + 'is undefined');
-        return;
-    }
-    var responder = p.reqq[this.seq];
-    delete p.reqq[this.seq];
-    responder(this);
 }
 
 //////////////////////////////////////////////////// Utility functions
@@ -234,8 +238,8 @@ function petrelUnmarshal(p, msgBlob) {
             macReader.readAsText(macBlob);
         }
         // launch the payload handler
-        payloadRaw = msgBlob.slice(payloadStart, payloadStart + msg.plen);
-        payloadReader = new FileReader();
+        var payloadRaw = msgBlob.slice(payloadStart, payloadStart + msg.plen);
+        var payloadReader = new FileReader();
         payloadReader.onload = function(evt) {
             msg.payload = evt.target.result;
             msg.rebuild(p);
